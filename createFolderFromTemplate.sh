85afe2e02dfc8cad34da5b88bbdc64d1ce332ed3
Dave <Dgoldstein01@gmail.com>
2017-10-09 11:24:27 -0400

    updated template and removed templated folders

--- createFolderFromTemplate.sh
+++ createFolderFromTemplate.sh
@@ -12,10 +12,12 @@ else # directory does not exist we're good
 	cp -r template_dir/* ${1}
 	# rename files to reflect new number
 	mv ${1}/problem_x.c ${1}/problem_${1}.c
-	mv ${1}/problem_x.html ${1}/problem_${1}.html
-	# copy html to the text file
-	curl "https://projecteuler.net/problem=${1}" >> ${1}/problem_${1}.html
+	# download html to the text file
+	curl "https://projecteuler.net/problem=${1}" >> ${1}/problem_${1}.html	
+	# parse html and copy to text file
+	w3m ${1}/problem_${1}.html | less >> ${1}/problem_${1}.txt
+	rm ${1}/problem_${1}.html # remove html
 	#copy over problem number to new makefile
-	echo 'PROBLEM_NUMBER='${1}''$'\n'"$(cat ${1}/makefile)" > ${1}/makefile
+	echo 'PROBLEM_NUMBER='${1}''$'\n'"$(cat ${1}/makefile)" > ${1}/makefile	
 
 fi
