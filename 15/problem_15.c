64ee95c553c013759137d94455f64e4ba7d67581
David Goldstein <dgoldstein01@gmail.com>
2017-11-06 20:44:05 -0500

    solved 15

--- 15/problem_15.c
+++ 15/problem_15.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
 
+/**
+ * recursive helper for factorial
+ **/
+double factorialHelper(int n, double acc){
+	if (n == 1) return acc;
+	return factorialHelper(n-1, n * acc);
+}
+
+/**
+ * finds factorial of n
+ **/
+double factorial(int n){
+	return factorialHelper(n, 1);
+}
+
+
+/**
+ * (2n)!/n!×n!  
+ * @param {int} size of grid (number of Rs and Ds)
+ * @return {long} number of combinations
+ **/
+double binomialCoeff(int n) {
+	return factorial( 2 * n)  / (factorial(n) * factorial(n));
+}
+
 int main() {
+	printf("%lf\n",binomialCoeff(20));
 	return 0;
 }
