dfb0fde67e25fd7cd6ad369bf87d4cfd6b65830c
dgoldstein1 <david.goldstein@deciphernow.com>
2019-09-07 11:19:56 -0400

    able to make joined together

--- 30/problem_30.c
+++ 30/problem_30.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <math.h>
 
-char isSumOfFithPower(int n[], int length) {
-	double powerSum = 0;
-	double digitSum = 0;
-	for (int i = 0; i < length; i++) {
-		digitSum += (double) n[i];
-
+// checks if
+//   n[0]**5 + n[1]**5 ... n[j]**5 
+//      equals number 
+//   n[0:j] joined together
+char isSumOfFithPower(double n[], int length) {
+	double nJoinedTogether = 0;
+	// start at end	
+	for (int i = length - 1; i >= 0; i--) {
+		nJoinedTogether += pow(10, i) * n[i];
 	}
-	return powerSum == digitSum;
+	printf("%lf\n", nJoinedTogether);
+
+
+	return 1;
 }
 
 int main() {
-	printf("%b\n", isSumOfFithPower());
+	double test[] = {1,2,3,4,5,6};
+	int length = 6;
+	printf("%d\n", isSumOfFithPower(test, length));
 	return 0;
 }
